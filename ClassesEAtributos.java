5effda20795932ccaf0d9425c3e122b7971a6df0
Gabriel Reis <gaelsreis@hotmail.com>
2023-02-26 20:47:33 -0300

    06_poo-exercicio_pratico_2

---
+++ ClassesEAtributos.java
@@ -0,0 +1,14 @@
+//package CAPITULO_6;
+
+public class ClassesEAtributos {
+    //Atributos
+    int Idade = 5;
+
+    public static void main(String[] args) {
+        //objeto - instância
+
+        ClassesEAtributos Obj = new ClassesEAtributos();
+        Obj.Idade = 25;
+        System.out.println("Idade: " + Obj.Idade);
+    }
+}
